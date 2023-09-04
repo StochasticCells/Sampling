372b539c400e09d04755ec1a84ef37e3aa9a1545
Junzhuo <junzhuo@Junzhuos-MacBook-Pro.local>
2023-09-04 16:45:50 +0800

    add code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module statastical
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,12 @@
+package sampling
+
+type Variation struct {
+	Mean         float64
+	StdDeviation float64
+	Skewness     float64
+	Absulte      float64
+}
+
+func Sampling(variations []Variation, sampleNum int) (samples [][]float64) {
+	return
+}
